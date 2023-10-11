e10833703210265067f72d6f23e6354f48674b71
fedax01 <danielferenczi900@gmail.com>
2023-10-11 12:59:26 +0200

    Inmemory database setup, adding test data, change to enitityframeworkcore

--- QERH6L_HFT_2023241/QERH6L_HFT_2023241.Repository/CinemaContext.cs
+++ QERH6L_HFT_2023241/QERH6L_HFT_2023241.Repository/CinemaContext.cs
@@ -1,7 +1,7 @@
-﻿using QERH6L_HFT_2023241.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using QERH6L_HFT_2023241.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +10,16 @@ namespace QERH6L_HFT_2023241.Repository
 {
     public class CinemaContext : DbContext
     {
-        public CinemaContext() : base("CinemaContext") { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "CinemaDb");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Cinema cinema = new Cinema() {name = "Malom Mozi", address = "Deák Ferenc utca", city = "Kecskemét" };
+            Cinema cinema2 = new Cinema() { name = "Sugár Mozi", address = "Pillangó utca", city = "Budapest" };
+            Cinema cinema3 = new Cinema() { name = "Cinema City", address = "Malom utca", city = "Kiskunhalas" };
+        }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Movie> Movies { get; set; }
        
